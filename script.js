29ebec3966688c98613852a8135fbad28e3f94c7
Tran Kim Nikoletta <trankimniki@gmail.com>
2020-06-06 21:04:46 +0200

    exercise

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 let myVariable = false;
 if (myVariable !== true) {
-  console.log('The condition has been evaluated to true');
+    console.log('The condition has been evaluated to true');
 }
+
+for (let i = 0; i <= 50; i++) { 
+    if (i % 3 === 0 && i % 5 == 0) console.log(i) 
+}
+
+for (let i = 1; i <= 20; i++) 
+    { console.log(i)
+};
+
+
+
+    let getGreetingTo = (a, b) => {
+        return a + b;
+    }
